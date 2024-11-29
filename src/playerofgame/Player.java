6376c20094539c0199a33b627ce6a348ab8dd170
BienKieu <bien14112005@gmail.com>
2024-11-29 18:03:53 +0700

    git push

--- src/playerofgame/Player.java
+++ src/playerofgame/Player.java
@@ -7,19 +7,23 @@ public class Player<T> {
 	protected String nameOfPlayer;
 	protected ArrayList<T> cardsInHand;
 
+	//Phương thức khởi dựng
 	public Player(String nameOfPlayer) {
 		this.nameOfPlayer = nameOfPlayer;
 		this.cardsInHand = new ArrayList<>();
 	}
 
+	// Các lá bài trên tay của người chơi
 	public ArrayList<T> getCardsInHand() {
 		return this.cardsInHand;
 	}
 
+	// Thêm 1 lá bài lên tay
 	public void addCard(T card) {
 		this.cardsInHand.add(card);
 	}
 
+	// Đánh 1 lá bài
 	public void dropCard(T card) {
 		Iterator<T> iterator = cardsInHand.iterator();
 		while (iterator.hasNext()) {
@@ -31,6 +35,7 @@ public class Player<T> {
 		}
 	}
 
+	// In ra các lá bài hiện có trên tay
 	public void printCardInHand() {
 		System.out.println(nameOfPlayer + "'s cards in hand:");
 		for (T card : cardsInHand) {
@@ -39,10 +44,12 @@ public class Player<T> {
 		System.out.println();
 	}
 
+	// Lấy tên của người chơi
 	public String getNameOfPlayer() {
 		return nameOfPlayer;
 	}
 
+	// Set tên cho người chơi
 	public void setNameOfPlayer(String nameOfPlayer) {
 		this.nameOfPlayer = nameOfPlayer;
 	}
