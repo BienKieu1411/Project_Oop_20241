113a56f9c294314f31475452858ffaba599685a9
BienKieu <bien14112005@gmail.com>
2024-12-03 13:38:17 +0700

    git push

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import gamecardbaccarat.Baccarat;
 import gamecardthirteens.ThirteenS;
-
 import java.util.Scanner;
 
 public class Main{
@@ -8,8 +7,11 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("- Enter 1 to start a Baccarat game.");
         System.out.println("- Enter 2 to start a Thirteen game.");
-        System.out.print("Select game to start: ");
-        int selectedGame = scanner.nextInt();
+        int selectedGame;
+        do{
+            System.out.print("Select game to start: ");
+            selectedGame  = scanner.nextInt();
+        }while (selectedGame < 1 || selectedGame > 2);
         switch (selectedGame) {
             case 1:
                 Baccarat baccarat = new Baccarat();
