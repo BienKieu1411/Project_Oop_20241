7ee967f3437dd00f244d427f5d766076c558274a
Nehut141 <tuyendznha01gmail.com>
2024-12-04 17:08:50 +0700

    git push

--- src/gameplay/GameController.java
+++ src/gameplay/GameController.java
@@ -15,14 +15,14 @@ public class GameController {
         gameRoot.setPrefSize(1200, 675);
 
         // Placeholder for game interface
-        Button backToMainMenu = new Button("Main Menu");
-        backToMainMenu.setPrefSize(200, 50);
+            Button backToMainMenu = new Button("Quit");
+        backToMainMenu.setPrefSize(45, 15);
         backToMainMenu.setStyle("-fx-background-color: linear-gradient(to bottom, #F45A4A, #D93324); -fx-background-radius: 5; "
-                + "-fx-border-radius: 10; -fx-font-size: 16; -fx-font-weight: bold; -fx-text-fill: white;");
+                + "-fx-border-radius: 10; -fx-font-size: 8; -fx-font-weight: bold; -fx-text-fill: white;");
         backToMainMenu.setOnMouseClicked(event -> stage.setScene(new MainMenu().createMainMenu(stage)));
 
-        AnchorPane.setTopAnchor(backToMainMenu, 300.0);
-        AnchorPane.setLeftAnchor(backToMainMenu, 500.0);
+        AnchorPane.setTopAnchor(backToMainMenu, 0.0);
+        AnchorPane.setRightAnchor(backToMainMenu, 0.0);
 
         gameRoot.getChildren().add(backToMainMenu);
         stage.setScene(new Scene(gameRoot, 1200, 675));
